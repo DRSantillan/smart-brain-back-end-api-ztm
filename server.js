f6eb65b7f1e450fdb652c1bceadf9e0803d9cfa7
Dean <dean@deansantillan.com>
2021-12-26 08:10:45 +1000

    task(): setup express server and added signin and homepage route. tested on postman.

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+
+const apiPort = '8000';
+
+const app = express();
+app.use(express.json());
+const smartBrainDB = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: '123',
+			entries: 0,
+			joined: new Date(),
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: '123',
+			entries: 0,
+			joined: new Date(),
+		},
+	],
+};
+
+app.get('/', (req, res) => {
+	res.send('getting from the api server.');
+});
+
+app.post('/signin', (req, res) => {
+	console.log(req.body);
+	if (
+		req.body.email === smartBrainDB.users[0].email &&
+		req.body.password === smartBrainDB.users[0].password
+	) {
+		res.json('success');
+	} else {
+		res.status(400).json('error logging in');
+	}
+});
+
+app.post('/register', (req, res) => {
+	res.json('registered');
+});
+
+app.listen(8000, () => {
+	console.log(`app is running on port ${apiPort}`);
+});
+
+// /signin > post
+// /register > post
+// /profile/:userid > get > user
+// /image > put > user
