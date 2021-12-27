f85d61324f082aa8f770d2c86788dd3d157b0bdb
Dean <dean@deansantillan.com>
2021-12-27 17:17:07 +1000

    task(): found reference I didnt need

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,16 @@
 import Clarifai from 'clarifai';
-import { restart } from 'nodemon';
 // Clarifai Data for accessing the API
 const app = new Clarifai.App({
 	apiKey: '889a201a71cd40498bbe4ff050e39308',
 });
 export const handleApiCall = (req, res) => {
-	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-	.then(data => {res.json(data)})
-	.catch(error => res.status(400).json(error));
-}
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((error) => res.status(400).json(error));
+};
 
 export const handleImage = (postgresDB) => (req, res) => {
 	const { id } = req.body;
@@ -19,5 +21,3 @@ export const handleImage = (postgresDB) => (req, res) => {
 		.then((entries) => res.json(entries[0]))
 		.catch((error) => res.status(400).json('not updating'));
 };
-
-
